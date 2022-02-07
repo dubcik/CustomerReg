a04351418716122e08d44afae1df0a4e7405a1d3
Florin Dell <florin.dianoiait@outlook.com>
2022-02-07 15:31:54 +0100

    create the controller for API simulator

---
+++ src/DiaApiSimulator/Controllers/PublicApiSimulatorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DiaApiSimulator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublicApiSimulatorController : ControllerBase
+    {
+        [HttpPost("{requestUid}/PostRequest")]
+        public ActionResult AddPostRequest([FromRoute]Guid requestUid)
+        {
+            return Ok(requestUid);
+        }
+    }
+}

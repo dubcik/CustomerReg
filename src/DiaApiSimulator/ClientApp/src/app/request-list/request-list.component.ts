22a80000b1ed25daf3e8866ee6e832208aeede63
dubcik <dubcik92@gmail.com>
2022-02-17 21:22:36 +0200

    try to parse using regex

--- src/DiaApiSimulator/ClientApp/src/app/request-list/request-list.component.ts
+++ src/DiaApiSimulator/ClientApp/src/app/request-list/request-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ComunicationWithServerService } from '../comunication-with-server/comunication-with-server.service';
 import { DataFormFromServer } from '../comunication-with-server/comunication-with-server.model';
+import { forEachChild } from 'typescript';
 
 @Component({
   selector: 'app-request-list',
@@ -13,11 +14,14 @@ export class RequestListComponent implements OnInit {
   constructor(public service: ComunicationWithServerService) { }
 
   listOfRequest:DataFormFromServer[] = [];
-  
   contentOfOneRequest:string[]=[];
+  disableAllRequest:boolean;
+  listOfRequestBySerch:DataFormFromServer[] = [];
+  searchValue:string;
   
   ngOnInit() 
   {
+    this.disableAllRequest=true;
     this.getDataFromService();
   }
  
@@ -29,4 +33,30 @@ export class RequestListComponent implements OnInit {
                       this.listOfRequest = res    ), 
                  (error: any) => console.error(error); 
   }
-}
+
+  onSerchButtonClick()
+  {
+    return this.searchValue;
+  }
+  findElementInListOfRequest()
+  {
+    this.disableAllRequest = false;
+    console.log(this.searchValue);
+    this.listOfRequestBySerch = this.listOfRequest.
+            filter(
+                      listOfRequest=>
+                      listOfRequest.key == this.searchValue
+                  );
+                  this.findBySearch(this.searchValue)
+  }
+
+  findBySearch(params:string)
+  {
+     var pet = "ffff";
+     let regex = new RegExp ('/^((?!('+pet+')).)*$/');
+     console.log(regex.test(params));
+  }
+} 
+   
+    
+  
